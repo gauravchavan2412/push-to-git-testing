174d543fd82fe5ea868b0367719a95a2840239e1
stackgen-automation <automation@stackgen.com>
2024-12-11 10:54:36 +0000

    Automated commit

---
+++ terraform/outputs.tf
@@ -0,0 +1,99 @@
+
+output "aws_lambda_stackgen_d012de_function_arn" {
+  value = module.stackgen_6f36f35b-9818-5ad3-9b18-0593c09368e4.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_d012de_function_function_arn" {
+  value = module.stackgen_6f36f35b-9818-5ad3-9b18-0593c09368e4.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_d012de_function_function_name" {
+  value = module.stackgen_6f36f35b-9818-5ad3-9b18-0593c09368e4.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_d012de_function_function_url" {
+  value = module.stackgen_6f36f35b-9818-5ad3-9b18-0593c09368e4.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_d012de_function_invoke_arn" {
+  value = module.stackgen_6f36f35b-9818-5ad3-9b18-0593c09368e4.invoke_arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_stackgen_d012de_role_arn" {
+  value = module.stackgen_78d13fd8-6810-5c74-94a9-c43d8977960f.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_d012de_role_name" {
+  value = module.stackgen_78d13fd8-6810-5c74-94a9-c43d8977960f.name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_d012de_function_arn" {
+  value = module.stackgen_e136cecf-1e1a-5600-88ab-a89541006a75.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_d012de_function_name" {
+  value = module.stackgen_e136cecf-1e1a-5600-88ab-a89541006a75.name
+  sensitive = false
+}
+
+
+output "aws_s3_import_topology_arn" {
+  value = module.stackgen_aaace795-24e8-4496-8c5b-fe1642e246fb.arn
+  sensitive = false
+}
+
+output "aws_s3_import_topology_bucket_name" {
+  value = module.stackgen_aaace795-24e8-4496-8c5b-fe1642e246fb.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_import_topology_bucket_website_endpoint" {
+  value = module.stackgen_aaace795-24e8-4496-8c5b-fe1642e246fb.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_import_topology_kms_arn" {
+  value = module.stackgen_aaace795-24e8-4496-8c5b-fe1642e246fb.kms_arn
+  sensitive = false
+}
+
+
+output "aws_rds_default_rds_endpoint" {
+  value = module.stackgen_6b395d9e-6ca8-402a-9a7c-5c653c2e1c4f.rds_endpoint
+  sensitive = false
+}
+
+output "aws_rds_default_username" {
+  value = module.stackgen_6b395d9e-6ca8-402a-9a7c-5c653c2e1c4f.username
+  sensitive = false
+}
+
+output "aws_rds_default_arn" {
+  value = module.stackgen_6b395d9e-6ca8-402a-9a7c-5c653c2e1c4f.arn
+  sensitive = false
+}
+
+output "aws_rds_default_connection_string" {
+  value = module.stackgen_6b395d9e-6ca8-402a-9a7c-5c653c2e1c4f.connection_string
+  sensitive = true
+}
+
+output "aws_rds_default_password" {
+  value = module.stackgen_6b395d9e-6ca8-402a-9a7c-5c653c2e1c4f.password
+  sensitive = true
+}
+
+output "aws_rds_default_port" {
+  value = module.stackgen_6b395d9e-6ca8-402a-9a7c-5c653c2e1c4f.port
+  sensitive = false
+}
