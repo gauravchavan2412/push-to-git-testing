2d7c640b95c3486162add454f1bd0cc67f032450
stackgen-automation <automation@stackgen.com>
2025-01-30 06:34:20 +0000

    Automated commit

---
+++ terraform/outputs.tf
@@ -0,0 +1,21 @@
+
+output "aws_iam_role_somevalue_arn" {
+  value = module.stackgen_a5823e5c-54e8-4695-bf42-499d19009201.arn
+  sensitive = false
+}
+
+output "aws_iam_role_somevalue_name" {
+  value = module.stackgen_a5823e5c-54e8-4695-bf42-499d19009201.name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_2f8e50_function_arn" {
+  value = module.stackgen_37c2b353-d516-5004-a8ae-4e34661833c8.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_2f8e50_function_name" {
+  value = module.stackgen_37c2b353-d516-5004-a8ae-4e34661833c8.name
+  sensitive = false
+}
